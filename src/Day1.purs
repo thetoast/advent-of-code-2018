a3f259e0d37088c2e84307e210e900dd054a58c2
Ryan Mechelke <rfmechelke@gmail.com>
2018-12-01 12:12:14 -0600

    day 1 part 1

--- src/Day1.purs
+++ src/Day1.purs
@@ -1,10 +1,33 @@
 module Day1 where
 
 import Prelude
+import Data.Int as Int
+import Data.Foldable (foldl)
+import Data.Maybe (Maybe(..))
+import Data.String as String
+import Data.String (Pattern(..))
 import Effect (Effect)
 import Effect.Console (log)
+
+type Op = Int -> Int
+
+toOp :: String -> Op
+toOp input = do
+    let { before, after } = String.splitAt 1 input
+    let afterInt = Int.fromString after
+    case afterInt of
+        Nothing -> (+) 0
+        Just x -> case before of
+            "-" -> flip (-) $ x
+            _ -> (+) x
+
+foldOps :: Array Op -> Int
+foldOps = foldl (\acc op -> op acc) 0
+
+solve :: String -> Int
+solve input = foldOps $ toOp <$> String.split (Pattern "\n") input
 
 day1 :: String -> Effect String
 day1 input = do
     log "Day 1 started"
-    pure $ "Hey! " <> input <> " is the result for Day 1!"
+    pure $ show $ solve input
--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,9 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
+
+import Test.Day1 as Day1
 
 main :: Effect Unit
 main = do
-  log "You should add some tests."
+    Day1.main
