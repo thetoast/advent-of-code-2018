c1e67900719c1d64a79a9102558cf032d9930ba9
Ryan Mechelke <rfmechelke@gmail.com>
2018-12-21 00:20:20 -0600

    add day7

---
+++ src/Day7.purs
@@ -0,0 +1,12 @@
+module Day7 where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Util (Solution, Program)
+
+part1 :: Program Solution
+part1 = pure $ Just "Part 1"
+
+part2 :: Program Solution
+part2 = pure $ Just "Part 2"
